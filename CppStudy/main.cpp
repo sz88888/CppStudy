dd0b6bcfd2d51aa51a1c92d73f25c76cb13fe9a7
sz <shizhan96@gmail.com>
2019-07-14 20:38:53 +0100

    Initial Commit

---
+++ CppStudy/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CppStudy
+//
+//  Created by zsz on 2019/7/14.
+//  Copyright © 2019 zsz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
